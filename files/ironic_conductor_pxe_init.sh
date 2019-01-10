5ffb9f38240ab3f6d023e0cf0f0d33ad9e91fb05
Yolanda Robla <yroblamo@redhat.com>
2019-01-10 13:47:43 +0100

    Fix bug in path for pxe

--- files/ironic_conductor_pxe_init.sh
+++ files/ironic_conductor_pxe_init.sh
@@ -6,6 +6,6 @@ mkdir -p /var/lib/pod_data/tftpboot/master_images
 
 for FILE in undionly.kpxe ipxe.efi; do
   if [ -f /usr/share/ipxe/$FILE ]; then
-    cp -v /usr/lib/ipxe/$FILE /var/lib/pod_data/tftpboot
+    cp -v /usr/share/ipxe/$FILE /var/lib/pod_data/tftpboot
   fi
 done
